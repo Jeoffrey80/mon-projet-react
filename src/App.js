a1b032bc04957ec3234b9c54c269dceef37188e8
Jeoffrey80 <meyxana80@gmail.com>
2023-11-27 14:47:34 +0100

    tu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+function ListeCoursesApp() {
+  const [listeCourses, setListeCourses] = useState([]);
+  const [nouvelElement, setNouvelElement] = useState('');
+
+  const ajouterElement = () => {
+    if (nouvelElement.trim() !== '') {
+      setListeCourses([...listeCourses, nouvelElement]);
+      setNouvelElement('');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Liste de Courses</h2>
+
+      <div>
+        <input
+          type="text"
+          value={nouvelElement}
+          onChange={(e) => setNouvelElement(e.target.value)}
+        />
+        <button onClick={ajouterElement}>Ajouter</button>
+      </div>
+
+      <ul>
+        {listeCourses.map((element, index) => (
+          <li key={index}>{element}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 function App() {
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [compteur, setCompteur] = useState(0);
+
+  const getMessage = () => {
+    if (nom !== '' || prenom !== '') {
+      return `Bonjour ${prenom} ${nom}`;
+    } else {
+      return 'Veuillez remplir au moins l\'un des deux champs.';
+    }
+  };
+
+  const incrementerCompteur = () => {
+    setCompteur(compteur + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="nom">Nom :</label>
+      <input
+        type="text"
+        id="nom"
+        value={nom}
+        onChange={(e) => setNom(e.target.value)}
+      />
+
+      <br />
+
+      <label htmlFor="prenom">Prénom :</label>
+      <input
+        type="text"
+        id="prenom"
+        value={prenom}
+        onChange={(e) => setPrenom(e.target.value)}
+      />
+
+      <p>{getMessage()}</p>
+
+      <h1>Compteur : {compteur}</h1>
+      <button onClick={incrementerCompteur}>Incrémenter</button>
+
+      {/* Utilisation de la composante ListeCoursesApp */}
+      <ListeCoursesApp />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import axios from "axios";
 import App from './App';
 
 test('renders learn react link', () => {
@@ -6,3 +7,13 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
